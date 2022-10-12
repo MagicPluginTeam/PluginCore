f4dd0f1846ca62549c6b0cf8658e0a3f8a1a8230
Bruce0203 <BruceFreedy@gmail.com>
2022-10-12 21:17:48 +0900

    feat: add guiutils

---
+++ src/main/java/gui/TestCode.java
@@ -0,0 +1,17 @@
+package gui;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+
+public class TestCode {
+    public static void testCodeUsage(Plugin plugin, Player player) {
+        ItemStack apple = new ItemStack(Material.APPLE);
+        Gui.frame(plugin, 6, "Hello")
+                .slot(0, 0, apple, event -> event.getWhoClicked().sendMessage("Yo!"))
+                .slot(0, 1, event -> event.getWhoClicked().sendMessage("Yeah!"))
+                .openInventory(player);
+    }
+
+}
