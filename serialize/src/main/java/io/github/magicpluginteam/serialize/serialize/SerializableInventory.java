d368806294e52999f6db81783b367a5ef376e74e
Bruce0203 <BruceFreedy@gmail.com>
2022-10-23 00:50:31 +0900

    fix: note not implemented

--- serialize/src/main/java/io/github/magicpluginteam/serialize/serialize/SerializableInventory.java
+++ serialize/src/main/java/io/github/magicpluginteam/serialize/serialize/SerializableInventory.java
@@ -2,22 +2,19 @@ package io.github.magicpluginteam.serialize.serialize;
 
 import io.github.magicpluginteam.serialize.YamlSerialize;
 import io.github.magicpluginteam.serialize.symbol.YamlSymbol;
-import org.bukkit.Bukkit;
+import org.apache.commons.lang.NotImplementedException;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.Inventory;
 
 @YamlSymbol(symbol = "inventories")
 public class SerializableInventory implements YamlSerialize<Inventory> {
     @Override
     public Inventory deserialize(ConfigurationSection conf) {
-        return Bukkit.createInventory(null, conf.getInt("InventorySize"));
+        throw new NotImplementedException();
     }
 
     @Override
     public ConfigurationSection serialize(Inventory inventory) {
-        var section = new YamlConfiguration();
-        section.set("Material", inventory.getSize());
-        return section;
+        throw new NotImplementedException();
     }
 }
