ab08834d02e1f0fd4c8e6bd932e9fb0766d75965
Bruce0203 <BruceFreedy@gmail.com>
2022-10-23 14:58:18 +0900

    chore: publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+typealias ShadowJar = com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 val kotlin_version = "1.7.0"
 buildscript {
     repositories {
@@ -19,7 +21,7 @@ allprojects {
     apply(plugin = "org.gradle.java")
     apply(plugin = "org.gradle.java-library")
     apply(plugin = "com.github.johnrengelman.shadow")
-    tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    tasks.withType<ShadowJar> {
         archiveFileName.set("${rootProject.name}.jar")
     }
 
@@ -42,5 +44,47 @@ allprojects {
         compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
 
     }
+
+
 }
+
+lateinit var sourcesArtifact: PublishArtifact
+
+tasks {
+    val sourcesJar by creating(ShadowJar::class)
+    artifacts {
+        sourcesArtifact = archives(sourcesJar)
+    }
+}
+
+dependencies {
+    allprojects.forEach { api(it) }
+}
+
+publishing {
+    val repo = System.getenv("GITHUB_REPOSITORY")
+    if (repo === null) return@publishing
+    repositories {
+        maven {
+            url = uri("https://s01.oss.sonatype.org/content/repositories/releases/")
+            credentials {
+
+                username = System.getenv("SONATYPE_USERNAME") as? String
+                password = System.getenv("SONATYPE_PASSWORD") as? String
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>(project.name) {
+            val githubUserName = repo.substring(0, repo.indexOf("/"))
+            groupId = "io.github.${githubUserName.toLowerCase()}"
+            artifactId = project.name.toLowerCase()
+            version = System.getenv("GITHUB_BUILD_NUMBER")?: project.version.toString()
+            artifact(sourcesArtifact)
+        }
+    }
+
+}
+
+
 
